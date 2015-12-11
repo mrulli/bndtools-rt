98746abeeb4d0b681dcd113c0ebab5a15d7219cc
matteorulli <matteo.rulli@gmail.com>
2015-12-11 10:33:05 +0100

    Updated to v. 6 - added LogService usage

--- org.bndtools.rt.caffeine/src/org/bndtools/rt/caffeine/CaffeineActivator.java
+++ org.bndtools.rt.caffeine/src/org/bndtools/rt/caffeine/CaffeineActivator.java
@@ -6,21 +6,33 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleEvent;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.log.LogService;
 import org.osgi.util.tracker.BundleTracker;
 
 public class CaffeineActivator implements BundleActivator {
 
-	private BundleTracker tracker;
-
+	private BundleTracker<?> tracker;	
+	private LogService log = null;
+	
 	@Override
 	public void start(BundleContext context) throws Exception {
-		tracker = new BundleTracker(context, Bundle.STARTING, null) {
+		tracker = new BundleTracker<Object>(context, Bundle.STARTING, null) {
 			public Object addingBundle(Bundle bundle, BundleEvent event) {
+				
+				if(log == null){
+					ServiceReference<?> logRef = context.getServiceReference(LogService.class.getName());
+					if (logRef != null) 
+						log = (LogService) context.getService(logRef);
+				}
+
 				if (Constants.ACTIVATION_LAZY.equals(bundle.getHeaders().get(Constants.BUNDLE_ACTIVATIONPOLICY))) {
 					try {
+						if(log != null) log.log(LogService.LOG_INFO, "Coercing lazy bundle to start...");
 						bundle.start();
+						if(log != null) log.log(LogService.LOG_INFO, "... " + getBundleInfo(bundle) + " started!");
 					} catch (BundleException e) {
-						e.printStackTrace();
+						if(log != null) log.log(LogService.LOG_DEBUG, e.getMessage());
 					}
 					return bundle;
 				}
@@ -35,4 +47,8 @@ public class CaffeineActivator implements BundleActivator {
 		tracker.close();
 	}
 
+	private String getBundleInfo(final Bundle b){
+		return b.getSymbolicName() + ";version='" + b.getVersion().toString() + "'";
+	}
+	
 }
